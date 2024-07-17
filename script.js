d5e734facaa302030394ab8b616674244f7860b7
EEOROKU <113193015+EEOROKU@users.noreply.github.com>
2024-07-16 23:32:37 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-//load window at top
+// Load window at top
 window.onload = function() {
     window.scrollTo(0, 0);
 }
 
-//opens Navigation
+// Opens Navigation from the right
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("mySidenav").style.transform = "translateX(0)";
 }
 
+// Closes Navigation, sliding it out to the right
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("mySidenav").style.transform = "translateX(-100%)";
+    document.getElementById("mySidenav").style.transform = "translateX(100%)";
 }
 
-//carousel code
